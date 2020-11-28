071fda92f936f6ab1e36a0d000d8767af0ed1074
RedHulk05 <66919841+RedHulk05@users.noreply.github.com>
2020-11-28 20:16:15 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# imports
+import pyautogui
+import keyboard
+import time
+
+# Variables definition
+On = True
+Resp = input('Quer começar?(s/n)')
+
+# Input Request
+if Resp == 's':
+    time.sleep(2.0)
+
+# Primary Loop
+    while True:
+
+# Defining close key
+        if keyboard.is_pressed('c'):
+            break
+
+# Defining On key
+        if keyboard.is_pressed('r'):
+            On = True
+
+# Secondary Loop
+        while On:
+
+# Click definition (You can decrease or increase he number of click by changing de clicks number, it´s not recommended more than 10)
+            pyautogui.click(pyautogui.position(), clicks=4)
+
+# Defining Off key
+            if keyboard.is_pressed('f'):
+                On = False
